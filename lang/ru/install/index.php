41907bbddae6fb8f2e0766bf006e896b69d42fb0
Виталий Акмашев <va@crtweb.ru>
2017-09-07 10:36:29 +0500

    add ci structure for repo

--- lang/ru/install/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$MESS['CREATIVE_FOUNDATION_MODULE_NAME'] = 'Creative foundation';
-$MESS['CREATIVE_FOUNDATION_MODULE_DESCRIPTION'] = 'Базовый набор компонентов для битрикс от Creative';
-$MESS['CREATIVE_FOUNDATION_MODULE_PARTNER_NAME'] = 'Creative';
